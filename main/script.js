b2cb13659483d8cb452c553921d6c1a2b006b4bf
Shiva Kumar <vanamalashivakumar24@gmail.com>
2023-12-29 20:36:44 +0530

    Basic Portfolio

---
+++ main/script.js
@@ -0,0 +1,11 @@
+let navLinks = document.querySelectorAll("nav ul li a");
+let listelements = document.querySelectorAll("nav ul li")
+
+navLinks.forEach(link => {
+    link.addEventListener("click", eve => {
+        listelements.forEach(li => {
+
+        })
+    })
+})
+
