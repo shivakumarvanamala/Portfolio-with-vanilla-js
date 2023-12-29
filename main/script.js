db9b67dbd97b0d306d964036814885b642fe6383
Shiva Kumar <vanamalashivakumar24@gmail.com>
2023-12-30 01:59:57 +0530

    Made some changes in home page and some other also

--- main/script.js
+++ main/script.js
@@ -1,11 +1,39 @@
-let navLinks = document.querySelectorAll("nav ul li a");
-let listelements = document.querySelectorAll("nav ul li")
+function navigation() {
 
-navLinks.forEach(link => {
-    link.addEventListener("click", eve => {
-        listelements.forEach(li => {
 
-        })
+    document.querySelector(".arrow-1").addEventListener("click", (eve) => {
+        eve.preventDefault();
+        document.location.href = "#about";
     })
-})
+    document.querySelector(".arrow-2").addEventListener("click", (eve) => {
+        eve.preventDefault();
+        document.location.href = "#experience";
+    })
+    document.querySelector(".arrow-3").addEventListener("click", (eve) => {
+        eve.preventDefault();
+        document.location.href = "#projects";
+    })
+    document.querySelector(".arrow-4").addEventListener("click", (eve) => {
+        eve.preventDefault();
+        document.location.href = "#contact";
+    })
+    document.querySelector(".arrow-5").addEventListener("click", (eve) => {
+        eve.preventDefault();
+        document.location.href = "#home";
+    })
+}
+navigation();
 
+
+// FIXME : Check why below code isn't working with unexpected errors which are not correct
+
+
+// let sections = [about, experience, projects, contact]
+// let count = 1
+// document.querySelectorAll("arrow").forEach(ele, () => {
+//     // document.location.href = `#{section[count]}`
+//     ele.addEventListener("click", (eve) = {
+//         console.log(sections[count]);
+//         count ++;
+// })
+// })
